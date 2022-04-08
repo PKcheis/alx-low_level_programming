1cb67c01990d615b4cbf983ab4306dd27d7743e6
PKcheis <philipkubaichege@gmail.com>
2022-04-07 23:32:36 -0700

    use of puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,13 +4,10 @@
 
 /**
  *
- * * main - print the string in the put function
+ * main - print the string in the put function
+ * Description: using the main function
  *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
+ * this program prints "Programming is like building a multilingual puzzle
  *
  * Return: 0
  *
@@ -20,7 +17,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		puts("Programming is like building a multilingual puzzle");
 
 			return (0);
 
