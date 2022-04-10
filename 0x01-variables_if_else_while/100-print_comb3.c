b43fc16137ef695a16aa0f35806ca64816d099c0
PKcheis <philipkubaichege@gmail.com>
2022-04-10 04:22:05 -0700

    ge

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,26 +12,52 @@ int main(void)
 
 {
 
-	int c = 0;
-	int f_d;
-	int l_d;
-	while (c <= 99)
-	{		
-		f_d = (c / 10 + '0');
-		l_d = (c % 10 + '0');
-			if (f_d < l_d);
-																				{																		putchar(f_d);
-																					putchar(l_d);			
-																						if (f_d < l_d)
-																							{
-																								putchar(',');																																	putchar(' ');															}	
-																				}
-		c++;
-		}
-putchar('\n');
+		int c = 0;
 
-return (0);
+			int f_d;
+
+				int l_d;
+
+
+
+					while (c <= 99)
+
+							{
+
+										f_d = (c / 10 + '0');
+
+												l_d = (c % 10 + '0');
+
+
+
+														if (f_d < l_d)
+
+																	{
+
+																					putchar(f_d);
+
+																								putchar(l_d);
+
+
+
+																											if (c != 89)
+
+																															{
+
+																																				putchar(',');
+
+																																								putchar(' ');
+
+																																											}
+
+																													}
+
+																c++;
+
+																	}
+
+					putchar('\n');
+
+					return (0);
 
 }
-
-
