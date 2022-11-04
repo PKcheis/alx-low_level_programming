f404c7381438231cdec32f073ec25035e1c1bf15
PKcheis <philipkubaichege@gmail.com>
2022-11-04 15:18:32 +0300

    search algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+void printy_array(int *array, size_t low, size_t high);
+
+#endif /* SEARCH_ALGOS */
